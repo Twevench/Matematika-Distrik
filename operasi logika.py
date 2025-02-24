50e9c75749655ee83c03faa971fc8ef0380e37e5
Qurratul Aini <qurratulaini270306@gmail.com>
2025-02-25 06:44:04 +0800

    Create operasi logika.py

---
+++ operasi logika.py
@@ -0,0 +1,40 @@
+# Membuat jdul Tabel
+print("TABEL KEBENARAN KONJUNGSI")
+print("_________________________")
+
+# #Menampilkan Header tabel
+print("P\t|Q\t|P AND Q")
+print("_________________________")
+# Menggunakan Operator logika 'AND'
+for P in [True, False]:
+    for Q in [True, False]:
+        konjungsi = P and Q
+        print(f"{P}\t|{Q}\t{konjungsi}")
+print()
+
+# Menbuat jdul Tabel
+print("TABEL KEBENARAN DISJUNGSI")
+print("_________________________")
+
+#Menampilkan Header tabel
+print("P\t|Q\t|P AND Q")
+print("_________________________")
+# Menggunakan Operator logika 'AND'
+for P in [True, False]:
+    for Q in [True, False]:
+        disjungsi = P or Q
+        print(f"{P}\t|{Q}\t{disjungsi}")
+print()
+
+# Menbuat jdul Tabel
+print("TABEL KEBENARAN NEGASI")
+print("_________________________")
+
+# #Menampilkan Header tabel
+print("P\t|Q\t|P AND Q")
+print("_________________________")
+# Menggunakan Operator logika 'AND'
+for P in [True, False]:
+    for Q in [True, False]:
+        negasi = not P or Q
+        print(f"{P}\t|{Q}\t|{negasi}")
